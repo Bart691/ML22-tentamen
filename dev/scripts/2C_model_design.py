2fd4433544feed9876e2fe8813f28c8f9b281cfc
Bart691 <bartbayens@hotmail.com>
2023-01-24 20:30:30 +0000

    Aanpassig num_layers

--- dev/scripts/2C_model_design.py
+++ dev/scripts/2C_model_design.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
             tunedir=presets.logdir,
             hidden=228,
             dropout=0.18,
-            num_layers=5,
+            num_layers=4,
         ),
     ]
 
